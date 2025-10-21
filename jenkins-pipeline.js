66ed7b39ab0644980a217df18f6fbc0e3321c5dc
M Hasan <hasanmuhammad197@gmail.com>
2025-10-21 19:41:05 +0700

    Pipeline concept

--- jenkins-pipeline.js
+++ jenkins-pipeline.js
@@ -10,3 +10,14 @@
 //Jenkins Pipeline Instalation
 //Saat pertama install jenkins, secara default jenkins pipeline tidak terdapat di jenkins
 plugins.jenkins.io/workflow-aggregator/ //perlu install plugin pipeline terlebih dahulu atau lewat Plugin Manager
+
+//Pipeline Concept
+//Jenkins pipeline memiliki beberapa konsep dalam penggunaannya, yaitu:
+//1. Pipeline : definisi kode continous delivery, berisi seluruh instriksi untuk proses CD seperti compile, testing, deploy, dll
+//Kode pipeline menggunakan kata kunci pipeline
+//2. Agent : machine/server bagian dari jenkins yang digunakan untuk mengeksekusi pipeline
+//Kode penentuan Agent adalah kata kunci agent, Agent disebutkan di dalam kode pipeline yang dibuat
+//3. Stage : blok definisi tugas/tahapan dalam pipeline spt: 'Build', 'Test', 'Deploy' dll. Biasanya ditampilkan di jenkins seperti tahapan progress dari pipeline
+//Biasanya dalam pipeline akan terdapat banyak stage, Stage menggunakan kata kunci stage
+//4. Step adalah sebuah instruksi/perintah yg harus dilakukan oleh jenkins
+//Step dilakukan di dalam Stage, Step menggunakan kata kunci steps
